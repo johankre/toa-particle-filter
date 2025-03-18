40f1c72173a477ebafe0837e7d5fa4743fd74ef6
johannes <johannes.k.reitan@gmail.com>
2025-03-18 22:08:19 +0100

    unit test: updated_est_position

--- particle_filter/swarm_element/src/swarm_element.rs
+++ particle_filter/swarm_element/src/swarm_element.rs
@@ -9,11 +9,17 @@ pub struct SwarmElement {
 }
 
 impl SwarmElement {
-    pub fn new(true_position: Vector3<f32>, num_particles: usize) -> Self {
+    pub fn new(
+        true_position: Vector3<f32>,
+        num_particles: usize,
+        x_bounds: (f32, f32),
+        y_bounds: (f32, f32),
+        z_bounds: (f32, f32),
+    ) -> Self {
         Self {
             true_position,
             est_position: Vector3::zeros(),
-            particles: Particles::new(num_particles, (0.0, 1.0), (0.0, 1.0), (0.0, 1.0)),
+            particles: Particles::new(num_particles, x_bounds, y_bounds, z_bounds),
         }
     }
 
@@ -35,8 +41,12 @@ mod tests {
     fn test_new_swarm_element() {
         let true_position = Vector3::new(0.5, 0.5, 0.5);
         let num_particles = 10;
+        let x_bounds = (0.0, 1.0);
+        let y_bounds = (0.0, 1.0);
+        let z_bounds = (0.0, 1.0);
 
-        let swarm_element = SwarmElement::new(true_position, num_particles);
+        let swarm_element =
+            SwarmElement::new(true_position, num_particles, x_bounds, y_bounds, z_bounds);
 
         assert_eq!(swarm_element.true_position, true_position);
         assert_eq!(swarm_element.est_position, Vector3::zeros());
@@ -46,5 +56,30 @@ mod tests {
             .particles
             .iter()
             .for_each(|p| assert_eq!(p.weight, 1.0 / (num_particles as f64)));
+    }
+
+    #[test]
+    fn test_update_est_position() {
+        let true_position = Vector3::new(0.5, 0.5, 0.5);
+        let num_particles = 10_000;
+        let x_bounds = (0.0, 1.0);
+        let y_bounds = (0.0, 2.0);
+        let z_bounds = (0.0, 3.0);
+
+        let mut swarm_element =
+            SwarmElement::new(true_position, num_particles, x_bounds, y_bounds, z_bounds);
+        swarm_element.update_est_position();
+
+        let tolerance = 0.01;
+
+        assert!(
+            (swarm_element.est_position.x - (x_bounds.1 - x_bounds.0) / 2.0).abs() <= tolerance
+        );
+        assert!(
+            (swarm_element.est_position.y - (y_bounds.1 - y_bounds.0) / 2.0).abs() <= tolerance
+        );
+        assert!(
+            (swarm_element.est_position.z - (z_bounds.1 - z_bounds.0) / 2.0).abs() <= tolerance
+        );
     }
 }
