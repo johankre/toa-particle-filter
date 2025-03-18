7e420fbb0ba92553511df8fe9963061ec9036482
johannes <johannes.k.reitan@gmail.com>
2025-03-18 21:45:17 +0100

    est_position swarm_element

--- particle_filter/swarm_element/src/swarm_element.rs
+++ particle_filter/swarm_element/src/swarm_element.rs
@@ -2,14 +2,16 @@ use crate::Particles;
 use nalgebra::Vector3;
 
 pub struct SwarmElement {
-    position: Vector3<f32>,
+    true_position: Vector3<f32>,
+    est_position: Vector3<f32>,
     particles: Particles,
 }
 
 impl SwarmElement {
-    pub fn new(position: Vector3<f32>, num_particles: usize) -> Self {
+    pub fn new(true_position: Vector3<f32>, num_particles: usize) -> Self {
         Self {
-            position,
+            true_position,
+            est_position: Vector3::zeros(),
             particles: Particles::new(num_particles, (0.0, 1.0), (0.0, 1.0), (0.0, 1.0)),
         }
     }
@@ -17,18 +19,17 @@ impl SwarmElement {
 
 #[cfg(test)]
 mod tests {
-    use rand::seq::IndexedRandom;
-
     use super::*;
 
     #[test]
     fn test_new_swarm_element() {
-        let position = Vector3::new(0.5, 0.5, 0.5);
+        let true_position = Vector3::new(0.5, 0.5, 0.5);
         let num_particles = 10;
 
-        let swarm_element = SwarmElement::new(position, num_particles);
+        let swarm_element = SwarmElement::new(true_position, num_particles);
 
-        assert_eq!(swarm_element.position, position);
+        assert_eq!(swarm_element.true_position, true_position);
+        assert_eq!(swarm_element.est_position, Vector3::zeros());
         assert_eq!(swarm_element.particles.particles.len(), 10);
         swarm_element
             .particles
