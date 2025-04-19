0896abc6067f0badb56b29e78dde9b432bf79928
johannes <johannes.k.reitan@gmail.com>
2025-04-19 16:07:30 +0200

    added toa mesurments trait

--- particle_filter/agents/src/anchor.rs
+++ particle_filter/agents/src/anchor.rs
@@ -1,4 +1,9 @@
+use crate::measurments::Measurments;
+use crate::swarm_element::SwarmElement;
+
 use nalgebra::Vector3;
+use rand::rng;
+use rand_distr::{Distribution, Normal};
 
 pub struct Anchor {
     position: Vector3<f32>,
@@ -12,9 +17,24 @@ impl Anchor {
     }
 }
 
+impl Measurments for Anchor {
+    fn time_of_arival_mesurment(
+        &self,
+        swarm_element: &SwarmElement,
+        measurement_std_deviation: f32,
+    ) -> Result<f32, rand_distr::NormalError> {
+        let normal_dist = Normal::new(0.0, measurement_std_deviation)?;
+        let diff = self.position - swarm_element.true_position;
+        Ok(diff.norm() + normal_dist.sample(&mut rng()))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::particle_filter::{BoundingBox, ParticleFilter};
+
+    use rayon::prelude::*;
 
     #[test]
     fn test_new_anchor() {
@@ -27,5 +47,58 @@ mod tests {
         assert_eq!(anchor.position.x, x);
         assert_eq!(anchor.position.y, y);
         assert_eq!(anchor.position.z, z);
+    }
+
+    #[test]
+    fn test_anchor_toa() {
+        let measurement_std_deviation = 0.1;
+
+        let anchor_x = 2.0;
+        let anchor_y = 0.0;
+        let anchor_z = 1.0;
+        let anchor = Anchor::new(anchor_x, anchor_y, anchor_z);
+
+        let true_position = Vector3::new(3.3, 2.2, 1.1);
+        let num_particles = 10;
+        let enclosure =
+            BoundingBox::new(Vector3::new(0.0, 0.0, 0.0), Vector3::new(5.0, 5.0, 5.0)).unwrap();
+        let particle_filter = ParticleFilter::new(&enclosure, num_particles);
+        let swarm_element = SwarmElement::new(true_position, particle_filter);
+
+        let num_samples = 100_000;
+        let empirical_sum: f32 = (0..num_samples)
+            .into_par_iter()
+            .map(|_| {
+                anchor
+                    .time_of_arival_mesurment(&swarm_element, measurement_std_deviation)
+                    .unwrap()
+            })
+            .sum();
+
+        let empirical_mean = empirical_sum / num_samples as f32;
+
+        let empirical_variance: f32 = (0..num_samples)
+            .into_par_iter()
+            .map(|_| {
+                let x = anchor
+                    .time_of_arival_mesurment(&swarm_element, measurement_std_deviation)
+                    .unwrap();
+                (x - empirical_mean).powi(2)
+            })
+            .sum::<f32>()
+            / num_samples as f32;
+        let expected_variance = measurement_std_deviation.powi(2);
+
+        let mean_tolerance = 0.01;
+        assert!(
+            (empirical_mean - (anchor.position - swarm_element.true_position).norm())
+                < mean_tolerance
+        );
+
+        let variance_tolerance = 0.01;
+        assert!(expected_variance - empirical_variance < variance_tolerance);
+
+        let is_nosiy = 0.0;
+        assert!(empirical_variance > is_nosiy);
     }
 }
