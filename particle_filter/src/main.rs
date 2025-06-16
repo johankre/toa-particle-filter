2078423f7dd0c61aa18db6b3e7999caa27f3fb84
johannes <johannes.k.reitan@gmail.com>
2025-06-16 17:02:47 +0200

    update main to readme example

--- particle_filter/src/main.rs
+++ particle_filter/src/main.rs
@@ -8,42 +8,37 @@ use simulation::simulation::SimulationBuilder;
 use visualization::visualization::RerunVisualization;
 
 fn main() {
-    let swarm_name_1 = String::from("swarm_element_1");
-
-    let sd_transmition_noise = 0.01;
-    let sd_ragning_noise = 0.4;
+    let swarm_name = String::from("swarm_element_1");
+    let sd_transmission_noise = 0.01;
+    let sd_ranging_noise = 0.4;
     let num_particles = 1_400_000;
-    let true_position_1 = Vector3::new(10.0, 10.0, 5.0);
-    let velocity_1 = Vector3::new(0.5, 0.5, 0.5);
-    let r = 20.0;
-    let origo = Vector3::new(10.0, 10.0, 5.0);
-    let spher = Spher::new(r, origo).unwrap();
-    let particle_filter_1 = ParticleFilter::new(&spher, num_particles);
+    let true_position = Vector3::new(10.0, 10.0, 5.0);
+    let velocity = Vector3::new(0.5, 0.5, 0.5);
+    let radius = 20.0;
+    let origin = Vector3::new(10.0, 10.0, 5.0);
 
-    let swarm_element_1 = SwarmElement::new(
-        swarm_name_1,
-        true_position_1,
-        particle_filter_1,
-        velocity_1,
-        sd_transmition_noise,
-        sd_ragning_noise,
+    let sphere = Spher::new(radius, origin).unwrap();
+    let particle_filter = ParticleFilter::new(&sphere, num_particles);
+
+    let swarm_element = SwarmElement::new(
+        swarm_name,
+        true_position,
+        particle_filter,
+        velocity,
+        sd_transmission_noise,
+        sd_ranging_noise,
     );
 
-    let anchor_std_ranging_1 = 0.1;
-    let anchor_position_1 = Vector3::new(0.0, 0.0, 0.0);
-    let anchor_1 = Anchor::new(anchor_position_1, anchor_std_ranging_1);
+    let anchor_std = 0.1;
+    let anchor1 = Anchor::new(Vector3::new(0.0, 0.0, 0.0), anchor_std);
+    let anchor2 = Anchor::new(Vector3::new(20.0, 20.0, 0.0), anchor_std);
 
-    let anchor_std_ranging_1 = 0.1;
-    let anchor_position_2 = Vector3::new(20.0, 20.0, 0.0);
-    let anchor_2 = Anchor::new(anchor_position_2, anchor_std_ranging_1);
-
-    let visualization_name = String::from("ToA-Particle_Filter");
-    let visualizer =
-        RerunVisualization::new(visualization_name).expect("Unable to create rerun visulization");
+    let visualizer = RerunVisualization::new(String::from("ToA-Particle-Filter"))
+        .expect("Unable to create rerun visualization");
 
     let mut sim = SimulationBuilder::default()
-        .swarm_elements(vec![swarm_element_1])
-        .anchors(vec![anchor_1, anchor_2])
+        .swarm_elements(vec![swarm_element])
+        .anchors(vec![anchor1, anchor2])
         .visualizer(visualizer)
         .build();
 
